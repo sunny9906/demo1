5724d97d46ce0eac7429ac774b25fdf41f9b9d14
sanketh <sankethsunny999@gmail.com>
2021-06-21 14:47:36 +0530

    created entity package and student class in that package

--- src/main/java/com/example1/demo1/entity/student.java
+++ src/main/java/com/example1/demo1/entity/student.java
@@ -1,4 +1,19 @@
 package com.example1.demo1.entity;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="stu")
+@Getter
+@Setter
 public class student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
 }
